8adbe66fe147c21108b3977e877fe06ed52664d6
redalban <jaudalban@hotmail.com>
2024-05-30 15:44:03 +0200

    zoomnavbar

--- code emploi/emploi.js
+++ code emploi/emploi.js
@@ -1,3 +1,15 @@
 function applyJob(jobTitle) {
     alert('Vous avez postulé pour le poste de ' + jobTitle);
 }
+$(document).ready(function(){
+    $('.image-container img').hover(
+        function() {
+            $(this).css('transform', 'scale(1.1)');
+
+        },
+        function() {
+            $(this).css('transform', 'scale(1)');
+
+        }
+    );
+});
--- code reseau/reseau.js
+++ code reseau/reseau.js
@@ -27,3 +27,16 @@ function filterSuggestions() {
         }
     });
 }
+
+$(document).ready(function(){
+    $('.image-container img').hover(
+        function() {
+            $(this).css('transform', 'scale(1.1)');
+
+        },
+        function() {
+            $(this).css('transform', 'scale(1)');
+
+        }
+    );
+});
