9a433a4e567911e5c91096338f458980f129b071
cocooooooooooooooooooooo <corentin.chapp@gmail.com>
2024-05-30 17:10:28 +0100

    modif pour maxence

--- CodeVous/script.js
+++ CodeVous/script.js
@@ -195,9 +195,11 @@ $(document).ready(function(){
     $('.image-container img').hover(
         function() {
             $(this).css('transform', 'scale(1.1)');
+            
         },
         function() {
             $(this).css('transform', 'scale(1)');
+            
         }
     );
 });
